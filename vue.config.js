5442e11f21faaff119acb4437279eaa91a67fe2e
Changsik00 <lowmans00@gmail.com>
2019-02-12 21:18:53 +0900

    eslint disable

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,7 @@
 // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint
 
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== "production",
-  devServer: {
-    overlay: {
-      warnings: false,
-      errors: true
-    }
+  chainWebpack: config => {
+    config.module.rules.delete("eslint");
   }
 };
