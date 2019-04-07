a863c38311c88011657196a54c94848b8586b4ea
Changsik00 <lowmans00@gmail.com>
2019-04-07 19:45:00 +0900

    config vue.js  load grobal scss -- webpack scss loader

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,44 @@
 // https://cli.vuejs.org/config/#lintonsave
 // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "static/assets"
+      : "http://0.0.0.0:8080/",
+  outputDir: "./dist/assets",
   chainWebpack: config => {
     config.module.rules.delete("eslint");
+    config.optimization.splitChunks(false);
+
+    config.plugin("BundleTracker").use(BundleTracker, [
+      {
+        filename: "webpack-stats.json"
+      }
+    ]);
+
+    config.resolve.alias.set("__STATIC__", "static/assets");
+
+    config.devServer
+      .public("http://0.0.0.0:8080")
+      .host("0.0.0.0")
+      .port(8080)
+      .hotOnly(true)
+      .watchOptions({
+        poll: 1000
+      })
+      .https(false)
+      .headers({
+        "Access-Control-Allow-Origin": ["*"]
+      });
+  },
+  // https://vueschool.io/articles/vuejs-tutorials/globally-load-sass-into-your-vue-js-applications/
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/assets/css/common.scss";`
+      }
+    }
   }
 };
