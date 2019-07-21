204fe1a3b7b8fdf3f6f09cdc7a6aa224048d8b82
Changsik00 <lowmans00@gmail.com>
2019-07-21 19:29:13 +0900

    bugfix -  login dialog not fullscreen issue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,122 +1,124 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-      <v-card>
-        <v-toolbar dark color="primary">
+  <v-dialog v-model="dialog" fullscreen scrollable transition="dialog-bottom-transition">
+    <v-card>
+      <v-toolbar dark color="primary">
+        <v-spacer></v-spacer>
+        <v-btn icon dark @click="dialog = false">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-layout
+        v-if="!showSignUp && !showFindPassword"
+        justify-center
+        column
+        style="width: 300px; margin: 100px auto;"
+      >
+        <h3 class="title" style="text-align: center">한 계정으로 모든 서비스 액세스</h3>
+        <form class="mt30" @submit.prevent="signInWithEmailAndPassword">
+          <b-field
+            label="Email"
+            :type="{'is-danger': errors.has('email')}"
+            :message="errors.first('email')"
+          >
+            <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
+          </b-field>
+
+          <b-field
+            label="Password"
+            :type="{'is-danger': errors.has('password')}"
+            :message="errors.first('password')"
+          >
+            <b-input
+              type="password"
+              v-model="password"
+              name="password"
+              v-validate="'required|min:8'"
+            />
+          </b-field>
+          <v-btn type="submit" class="mt30" block :loading="loading" color="secondary">로그인</v-btn>
+        </form>
+        <v-layout align-center class="mt20">
+          <div @click="showFindPassword = true" class="login-link">비밀번호를 잊으셨나요?</div>
           <v-spacer></v-spacer>
-          <v-btn icon dark @click="dialog = false">
-            <v-icon>close</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-layout
-          v-if="!showSignUp && !showFindPassword"
-          justify-center
-          column
-          style="width: 300px; margin: 100px auto;"
-        >
-          <h3 class="title" style="text-align: center">한 계정으로 모든 서비스 액세스</h3>
-          <form class="mt30" @submit.prevent="signInWithEmailAndPassword">
-            <b-field
-              label="Email"
-              :type="{'is-danger': errors.has('email')}"
-              :message="errors.first('email')"
-            >
-              <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
-            </b-field>
-
-            <b-field
-              label="Password"
-              :type="{'is-danger': errors.has('password')}"
-              :message="errors.first('password')"
-            >
-              <b-input
-                type="password"
-                v-model="password"
-                name="password"
-                v-validate="'required|min:8'"
-              />
-            </b-field>
-            <v-btn type="submit" class="mt30" block :loading="loading" color="secondary">로그인</v-btn>
-          </form>
-          <v-layout align-center class="mt20">
-            <div @click="showFindPassword = true" class="login-link">비밀번호를 잊으셨나요?</div>
-            <v-spacer></v-spacer>
-            <div @click="showSignUp = true" class="login-link">회원가입</div>
-          </v-layout>
-
-          <div class="mt30 line"></div>
-          <div class="mt30">
-            <div @click="authenticateSNS('google')" class="mt30 google-login"></div>
-          </div>
+          <div @click="showSignUp = true" class="login-link">회원가입</div>
         </v-layout>
-        <v-layout v-if="showSignUp" justify-center column style="width: 300px; margin:50px auto;">
-          <div class="login-link" @click="showSignUp = false">
-              < 로그인으로 이동
-          </div>
-          <form class="mt50" @submit.prevent="signUp">
-            <b-field
-              label="Email"
-              :type="{'is-danger': errors.has('email')}"
-              :message="errors.first('email')"
-            >
-              <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
-            </b-field>
 
-            <b-field
-              label="Password"
-              :type="{'is-danger': errors.has('password')}"
-              :message="errors.first('password')"
-            >
-              <b-input
-                type="password"
-                v-model="password"
-                name="password"
-                v-validate="'required|min:8'"
-              />
-            </b-field>
+        <div class="mt30 line"></div>
+        <div class="mt30">
+          <div @click="authenticateSNS('google')" class="mt30 google-login"></div>
+        </div>
+      </v-layout>
+      <v-layout v-if="showSignUp" justify-center column style="width: 300px; margin:50px auto;">
+        <div class="login-link" @click="showSignUp = false">< 로그인으로 이동</div>
+        <form class="mt50" @submit.prevent="signUp">
+          <b-field
+            label="Email"
+            :type="{'is-danger': errors.has('email')}"
+            :message="errors.first('email')"
+          >
+            <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
+          </b-field>
 
-            <b-field
-              label="Confirm password"
-              :type="{'is-danger': errors.has('confirm-password')}"
-              :message="[{
+          <b-field
+            label="Password"
+            :type="{'is-danger': errors.has('password')}"
+            :message="errors.first('password')"
+          >
+            <b-input
+              type="password"
+              v-model="password"
+              name="password"
+              v-validate="'required|min:8'"
+            />
+          </b-field>
+
+          <b-field
+            label="Confirm password"
+            :type="{'is-danger': errors.has('confirm-password')}"
+            :message="[{
               'The confirm password field is required' : errors.firstByRule('confirm-password', 'required'),
               'The confirm password is not valid' : errors.firstByRule('confirm-password', 'is')
             }]"
-            >
-              <b-input
-                type="password"
-                v-model="confirmPassword"
-                name="confirm-password"
-                v-validate="{ required: true, is: password }"
-              />
-            </b-field>
-            <v-btn type="submit" class="mt30" block :loading="loading" color="secondary">회원 가입</v-btn>
-          </form>
+          >
+            <b-input
+              type="password"
+              v-model="confirmPassword"
+              name="confirm-password"
+              v-validate="{ required: true, is: password }"
+            />
+          </b-field>
+          <v-btn type="submit" class="mt30" block :loading="loading" color="secondary">회원 가입</v-btn>
+        </form>
 
-          <div class="mt30 line"></div>
-          <div class="mt30">
-            <div @click="authenticateSNS('google')" class="mt30 google-login"></div>
-          </div>
-        </v-layout>
-        <v-layout v-if="showFindPassword" justify-center column style="width: 300px; margin:50px auto;">
-          <div class="login-link" @click="showFindPassword = false">
-              < 로그인으로 이동
-          </div>
-          <h3 class="mt30">등록된 이메일로 패스워드 변경<br>메일을 요청할 수 있습니다.</h3>
-          <form class="mt50" @submit.prevent="sendResetPassword">
-            <b-field
-              label="Email"
-              :type="{'is-danger': errors.has('email')}"
-              :message="errors.first('email')"
-            >
-              <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
-            </b-field>
-            <v-btn type="submit" class="mt30" block :loading="loading" color="secondary">요청</v-btn>
-          </form>
-        </v-layout>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+        <div class="mt30 line"></div>
+        <div class="mt30">
+          <div @click="authenticateSNS('google')" class="mt30 google-login"></div>
+        </div>
+      </v-layout>
+      <v-layout
+        v-if="showFindPassword"
+        justify-center
+        column
+        style="width: 300px; margin:50px auto;"
+      >
+        <div class="login-link" @click="showFindPassword = false">< 로그인으로 이동</div>
+        <h3 class="mt30">
+          등록된 이메일로 패스워드 변경
+          <br />메일을 요청할 수 있습니다.
+        </h3>
+        <form class="mt50" @submit.prevent="sendResetPassword">
+          <b-field
+            label="Email"
+            :type="{'is-danger': errors.has('email')}"
+            :message="errors.first('email')"
+          >
+            <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
+          </b-field>
+          <v-btn type="submit" class="mt30" block :loading="loading" color="secondary">요청</v-btn>
+        </form>
+      </v-layout>
+    </v-card>
+  </v-dialog>
 </template>
 <script>
 import firebase from "firebase";
