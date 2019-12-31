35117c76a0b0fbb280ee07902ba010e07532deaa
Changsik00 <lowmans00@gmail.com>
2019-12-31 14:41:04 +0900

    add policy info in login dialog

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,126 +5,171 @@
     hide-overlay
     transition="dialog-bottom-transition"
   >
-    <v-layout column style="padding: 0; height: 100%; background-color: white;">
+    <v-layout
+      column
+      style="padding: 0; height: 100%; background-color: white; overflow: hidden"
+    >
       <v-toolbar dark color="primary">
         <v-spacer></v-spacer>
         <v-btn icon dark @click="$store.state.showLogin = false">
           <v-icon>close</v-icon>
         </v-btn>
       </v-toolbar>
-      <div style="background-color: white;">
+      <div style="background-color: white; height: 100%; overflow-y: auto">
         <v-layout
           v-if="!showSignUp && !showFindPassword"
           justify-center
           column
-          style="width: 300px; margin: 100px auto 0 auto; padding-bottom: 100px;"
+          style="width: 300px; margin: 30px auto 0 auto; padding-bottom: 30px;"
         >
+          <div style="font-size: 12px; margin-top: 20px;">
+            * 로그인 함으로써
+            <a href="/policy/terms" class="login-policy-link">서비스이용약관</a
+            >&nbsp
+            <a href="/policy/privacy" class="login-policy-link"
+              >개인정보처리방침</a
+            >에 동의 합니다.
+          </div>
           <form class="mt30" @submit.prevent="authenticateEmail">
             <b-field
               label="Email"
-              :type="{'is-danger': errors.has('email')}"
+              :type="{ 'is-danger': errors.has('email') }"
               :message="errors.first('email')"
             >
-              <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
+              <b-input
+                v-model="email"
+                v-validate="'required|email'"
+                type="email"
+                name="email"
+              />
             </b-field>
 
             <b-field
               label="Password"
-              :type="{'is-danger': errors.has('password')}"
+              :type="{ 'is-danger': errors.has('password') }"
               :message="errors.first('password')"
             >
               <b-input
-                type="password"
                 v-model="password"
-                name="password"
                 v-validate="'required|min:8'"
+                type="password"
+                name="password"
               />
             </b-field>
-            <v-btn type="submit" class="mt30" block color="secondary">로그인</v-btn>
+            <v-btn type="submit" class="mt30" block color="secondary"
+              >로그인</v-btn
+            >
           </form>
           <v-layout align-center class="mt20">
-            <div @click="showFindPassword = true" class="login-link">비밀번호를 잊으셨나요?</div>
+            <div class="login-link" @click="showFindPassword = true">
+              비밀번호를 잊으셨나요?
+            </div>
             <v-spacer></v-spacer>
-            <div @click="showSignUp = true" class="login-link">회원가입</div>
+            <div class="login-link" @click="showSignUp = true">회원가입</div>
           </v-layout>
-
           <div class="mt30 line"></div>
-          <div style="display: flex; justify-content: space-between; padding: 30px;">
+          <div
+            style="display: flex; justify-content: space-between; padding: 30px;"
+          >
             <img
               class="icon-sns-login"
-              @click="authenticateSNS('facebook')"
               src="../assets/img/common/facebook-on.svg"
+              @click="authenticateSNS('facebook')"
             />
             <img
               class="icon-sns-login"
-              @click="authenticateSNS('twitter')"
               src="../assets/img/common/twitter-on.svg"
+              @click="authenticateSNS('twitter')"
             />
             <img
               class="icon-sns-login"
-              @click="authenticateSNS('google')"
               src="../assets/img/common/google.svg"
+              @click="authenticateSNS('google')"
             />
           </div>
         </v-layout>
-        <v-layout v-if="showSignUp" justify-center column style="width: 300px; margin:50px auto;">
-          <div class="login-link" @click="showSignUp = false">&lt; 로그인으로 이동</div>
+        <v-layout
+          v-if="showSignUp"
+          justify-center
+          column
+          style="width: 300px; margin:50px auto;"
+        >
+          <div class="login-link" @click="showSignUp = false">
+            &lt; 로그인으로 이동
+          </div>
           <form class="mt50" @submit.prevent="signUp">
             <b-field
               label="Email"
-              :type="{'is-danger': errors.has('email')}"
+              :type="{ 'is-danger': errors.has('email') }"
               :message="errors.first('email')"
             >
-              <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
+              <b-input
+                v-model="email"
+                v-validate="'required|email'"
+                type="email"
+                name="email"
+              />
             </b-field>
 
             <b-field
               label="Password"
-              :type="{'is-danger': errors.has('password')}"
+              :type="{ 'is-danger': errors.has('password') }"
               :message="errors.first('password')"
             >
               <b-input
-                type="password"
                 v-model="password"
-                name="password"
                 v-validate="'required|min:8'"
+                type="password"
+                name="password"
               />
             </b-field>
 
             <b-field
               label="Confirm password"
-              :type="{'is-danger': errors.has('confirm-password')}"
-              :message="[{
-              'The confirm password field is required' : errors.firstByRule('confirm-password', 'required'),
-              'The confirm password is not valid' : errors.firstByRule('confirm-password', 'is')
-            }]"
+              :type="{ 'is-danger': errors.has('confirm-password') }"
+              :message="[
+                {
+                  'The confirm password field is required': errors.firstByRule(
+                    'confirm-password',
+                    'required'
+                  ),
+                  'The confirm password is not valid': errors.firstByRule(
+                    'confirm-password',
+                    'is'
+                  )
+                }
+              ]"
             >
               <b-input
-                type="password"
                 v-model="confirmPassword"
-                name="confirm-password"
                 v-validate="{ required: true, is: password }"
+                type="password"
+                name="confirm-password"
               />
             </b-field>
-            <v-btn type="submit" class="mt30" block color="secondary">회원 가입</v-btn>
+            <v-btn type="submit" class="mt30" block color="secondary"
+              >회원 가입</v-btn
+            >
           </form>
 
           <div class="mt30 line"></div>
-          <div style="display: flex; justify-content: space-between; padding: 30px;">
+          <div
+            style="display: flex; justify-content: space-between; padding: 30px;"
+          >
             <img
               class="icon-sns-login"
-              @click="authenticateSNS('facebook')"
               src="../assets/img/common/facebook-on.svg"
+              @click="authenticateSNS('facebook')"
             />
             <img
               class="icon-sns-login"
-              @click="authenticateSNS('twitter')"
               src="../assets/img/common/twitter-on.svg"
+              @click="authenticateSNS('twitter')"
             />
             <img
               class="icon-sns-login"
-              @click="authenticateSNS('google')"
               src="../assets/img/common/google.svg"
+              @click="authenticateSNS('google')"
             />
           </div>
         </v-layout>
@@ -134,7 +179,9 @@
           column
           style="width: 300px; margin:50px auto;"
         >
-          <div class="login-link" @click="showFindPassword = false">&lt; 로그인으로 이동</div>
+          <div class="login-link" @click="showFindPassword = false">
+            &lt; 로그인으로 이동
+          </div>
           <h3 class="mt30">
             등록된 이메일로 패스워드 변경
             <br />메일을 요청할 수 있습니다.
@@ -142,12 +189,19 @@
           <form class="mt50" @submit.prevent="sendResetPassword">
             <b-field
               label="Email"
-              :type="{'is-danger': errors.has('email')}"
+              :type="{ 'is-danger': errors.has('email') }"
               :message="errors.first('email')"
             >
-              <b-input type="email" v-model="email" name="email" v-validate="'required|email'" />
+              <b-input
+                v-model="email"
+                v-validate="'required|email'"
+                type="email"
+                name="email"
+              />
             </b-field>
-            <v-btn type="submit" class="mt30" block color="secondary">요청</v-btn>
+            <v-btn type="submit" class="mt30" block color="secondary"
+              >요청</v-btn
+            >
           </form>
         </v-layout>
       </div>
@@ -303,5 +357,11 @@ export default {
   &:hover {
     color: #333333;
   }
+}
+
+.login-policy-link {
+  display: inline-block;
+  cursor: pointer;
+  border-bottom: 1px solid #333333;
 }
 </style>
