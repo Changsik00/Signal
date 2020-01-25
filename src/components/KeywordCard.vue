74d85da7bbe9dffd8fc0edb2d27f8c64d7815773
Changsik00 <lowmans00@gmail.com>
2020-01-25 15:05:21 +0900

    date 확인 후 today 표시

--- src/components/KeywordCard.vue
+++ src/components/KeywordCard.vue
@@ -5,21 +5,18 @@
       column
       style="padding: 20px; background-color: white"
     >
+      <div style="font-size: 18px; font-weight: bold;" v-html="item.title"></div>
       <div
-        style="font-size: 18px; font-weight: bold;"
-        v-html="item.title"
+        v-if="item.pubDate"
+        style="font-size: 14px; color: #9da6af"
+        v-html="dateToFormat(item.pubDate)"
       ></div>
-      <div v-if="item.pubDate" style="font-size: 14px; color: #9da6af">
-        {{ dateToFormat(item.pubDate) }}
-      </div>
-      <div v-if="item.postdate" style="font-size: 14px; color: #9da6af">
-        {{ dateToFormat2(item.postdate) }}
-      </div>
       <div
-        class="mt10"
-        style="word-break: break-all;"
-        v-html="item.description"
+        v-if="item.postdate"
+        style="font-size: 14px; color: #9da6af"
+        v-html="dateToFormat2(item.postdate)"
       ></div>
+      <div class="mt10" style="word-break: break-all;" v-html="item.description"></div>
       <div class="see-more">
         <a @click="seeMore">See More</a>
       </div>
@@ -68,6 +65,9 @@ export default {
     },
     dateToFormat(pubDate) {
       var d = new Date(pubDate);
+      const year = d.getFullYear();
+      const month = d.getMonth() + 1;
+      const day = d.getDate();
       return (
         d.getFullYear().toString() +
         "년" +
@@ -85,18 +85,35 @@ export default {
         ":" +
         ((parseInt(d.getMinutes() / 5) * 5).toString().length == 2
           ? (parseInt(d.getMinutes() / 5) * 5).toString()
-          : "0" + (parseInt(d.getMinutes() / 5) * 5).toString())
+          : "0" + (parseInt(d.getMinutes() / 5) * 5).toString()) +
+        this.checkToday(year, month, day)
       );
     },
     dateToFormat2(postdate) {
+      const year = postdate.substring(0, 4);
+      const month = postdate.substring(4, 6);
+      const day = postdate.substring(6, 8);
+
       return (
         postdate.substring(0, 4) +
         "년" +
         postdate.substring(4, 6) +
         "월" +
         postdate.substring(6, 8) +
-        "일 "
+        "일 " +
+        this.checkToday(year, month, day)
       );
+    },
+    checkToday(y, m, d) {
+      var now = new Date();
+      const year = now.getFullYear();
+      const month = now.getMonth() + 1;
+      const day = now.getDate();
+
+      if (y == year && m == month && d == day) {
+        return ' <span style="font-weight: bold; color: red"> Today </span>';
+      }
+      return "";
     }
   }
 };
