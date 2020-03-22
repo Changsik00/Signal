c874dc15c8cd603dc47faebee30581e0f07d5151
Changsik00 <lowmans00@gmail.com>
2020-03-22 14:14:28 +0900

    [refactoring] indent

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,10 +5,7 @@
     hide-overlay
     transition="dialog-bottom-transition"
   >
-    <v-layout
-      column
-      style="padding: 0; height: 100%; background-color: white; overflow: hidden"
-    >
+    <v-layout column style="padding: 0; height: 100%; background-color: white; overflow: hidden">
       <v-toolbar dark color="primary">
         <v-spacer></v-spacer>
         <v-btn icon dark @click="$store.state.showLogin = false">
@@ -24,11 +21,8 @@
         >
           <div style="font-size: 12px; margin-top: 20px;">
             * 로그인 함으로써
-            <a href="/policy/terms" class="login-policy-link">서비스이용약관</a
-            >&nbsp
-            <a href="/policy/privacy" class="login-policy-link"
-              >개인정보처리방침</a
-            >에 동의 합니다.
+            <a href="/policy/terms" class="login-policy-link">서비스이용약관</a>&nbsp
+            <a href="/policy/privacy" class="login-policy-link">개인정보처리방침</a>에 동의 합니다.
           </div>
           <form class="mt30" @submit.prevent="authenticateEmail">
             <b-field
@@ -36,12 +30,7 @@
               :type="{ 'is-danger': errors.has('email') }"
               :message="errors.first('email')"
             >
-              <b-input
-                v-model="email"
-                v-validate="'required|email'"
-                type="email"
-                name="email"
-              />
+              <b-input v-model="email" v-validate="'required|email'" type="email" name="email" />
             </b-field>
 
             <b-field
@@ -56,21 +45,15 @@
                 name="password"
               />
             </b-field>
-            <v-btn type="submit" class="mt30" block color="secondary"
-              >로그인</v-btn
-            >
+            <v-btn type="submit" class="mt30" block color="secondary">로그인</v-btn>
           </form>
           <v-layout align-center class="mt20">
-            <div class="login-link" @click="showFindPassword = true">
-              비밀번호를 잊으셨나요?
-            </div>
+            <div class="login-link" @click="showFindPassword = true">비밀번호를 잊으셨나요?</div>
             <v-spacer></v-spacer>
             <div class="login-link" @click="showSignUp = true">회원가입</div>
           </v-layout>
           <div class="mt30 line"></div>
-          <div
-            style="display: flex; justify-content: space-between; padding: 30px;"
-          >
+          <div style="display: flex; justify-content: space-between; padding: 30px;">
             <img
               class="icon-sns-login"
               src="../assets/img/common/facebook-on.svg"
@@ -88,27 +71,15 @@
             />
           </div>
         </v-layout>
-        <v-layout
-          v-if="showSignUp"
-          justify-center
-          column
-          style="width: 300px; margin:50px auto;"
-        >
-          <div class="login-link" @click="showSignUp = false">
-            &lt; 로그인으로 이동
-          </div>
+        <v-layout v-if="showSignUp" justify-center column style="width: 300px; margin:50px auto;">
+          <div class="login-link" @click="showSignUp = false">&lt; 로그인으로 이동</div>
           <form class="mt50" @submit.prevent="signUp">
             <b-field
               label="Email"
               :type="{ 'is-danger': errors.has('email') }"
               :message="errors.first('email')"
             >
-              <b-input
-                v-model="email"
-                v-validate="'required|email'"
-                type="email"
-                name="email"
-              />
+              <b-input v-model="email" v-validate="'required|email'" type="email" name="email" />
             </b-field>
 
             <b-field
@@ -147,15 +118,11 @@
                 name="confirm-password"
               />
             </b-field>
-            <v-btn type="submit" class="mt30" block color="secondary"
-              >회원 가입</v-btn
-            >
+            <v-btn type="submit" class="mt30" block color="secondary">회원 가입</v-btn>
           </form>
 
           <div class="mt30 line"></div>
-          <div
-            style="display: flex; justify-content: space-between; padding: 30px;"
-          >
+          <div style="display: flex; justify-content: space-between; padding: 30px;">
             <img
               class="icon-sns-login"
               src="../assets/img/common/facebook-on.svg"
@@ -179,9 +146,7 @@
           column
           style="width: 300px; margin:50px auto;"
         >
-          <div class="login-link" @click="showFindPassword = false">
-            &lt; 로그인으로 이동
-          </div>
+          <div class="login-link" @click="showFindPassword = false">&lt; 로그인으로 이동</div>
           <h3 class="mt30">
             등록된 이메일로 패스워드 변경
             <br />메일을 요청할 수 있습니다.
@@ -192,22 +157,16 @@
               :type="{ 'is-danger': errors.has('email') }"
               :message="errors.first('email')"
             >
-              <b-input
-                v-model="email"
-                v-validate="'required|email'"
-                type="email"
-                name="email"
-              />
+              <b-input v-model="email" v-validate="'required|email'" type="email" name="email" />
             </b-field>
-            <v-btn type="submit" class="mt30" block color="secondary"
-              >요청</v-btn
-            >
+            <v-btn type="submit" class="mt30" block color="secondary">요청</v-btn>
           </form>
         </v-layout>
       </div>
     </v-layout>
   </v-dialog>
 </template>
+
 <script>
 import firebase from "firebase";
 export default {
@@ -337,6 +296,7 @@ export default {
   }
 };
 </script>
+
 <style lang="scss">
 .sns-result {
   & > div {
