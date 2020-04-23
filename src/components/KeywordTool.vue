52ebf22cb37c6d1a9cc544a1d1f85f50872a456e
Dennis <dennis@genoplan.com>
2020-04-23 18:27:19 +0900

    keyword-tool > 임시커밋

--- src/components/KeywordTool.vue
+++ src/components/KeywordTool.vue
@@ -1,8 +1,6 @@
 실제 해당 키워드를 검색 했을때 온라인상에 노출되어지는 컨텐츠를 순서대로
 보여드려 경쟁 컨텐츠의 퀄리티를 확인할 수 있습니다.<template>
-  <section
-    style="background-color: white; width: 100%; height: 100%; overflow-y: auto"
-  >
+  <section style="background-color: white; width: 100%; height: 100%; overflow-y: auto">
     <section class="container">
       <div class="sg-title mt30 center">
         가장 가치있는
@@ -15,55 +13,80 @@
           <v-radio label="Instagram" value="Instagram"></v-radio>
           <v-radio label="Twitter" value="Twitter"></v-radio>
         </v-radio-group>
-        <div
-          style="display:flex; align-items: center; justify-content: center; margin-top: -10px"
-        >
-          <v-text-field
-            v-model="keyword"
-            solo
-            flat
-            class="search-form"
-            placeholder="키워드를 입력하세요."
-          ></v-text-field>
+        <div style="display:flex; align-items: center; justify-content: center; margin-top: -10px">
+          <v-text-field v-model="keyword" solo flat class="search-form" placeholder="키워드를 입력하세요."></v-text-field>
           <v-btn flat dark class="search-button" @click="search">검색</v-btn>
         </div>
         <!-- <div v-if="keyword == ''" style="margin-top: 80px;"> -->
         <div v-if="false" style="margin-top: 80px;">
           <KeywordToolInfo />
         </div>
-
-        <div>
-          <apexchart
-            height="350"
-            type="radialBar"
-            :options="chartOptions"
-            :series="series"
-          ></apexchart>
+        <div style="margin-top: 120px; display:flex; position: relative; align-items: center;">
+          <div class="top-chart-rect">
+            <div class="keyword-tool-label">
+              키워드 점수
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on">help</v-icon>
+                </template>
+                <div style="padding: 10px;">
+                  키워드검색량과 경쟁 컨텐츠량을 시그널의 알고리즘으로 계산하여 사용자가 알기쉽게 보여 드립니다.
+                  <br />*실시간데이터이기 때문에 점수가 변동될수있습니다.
+                </div>
+              </v-tooltip>
+            </div>
+            <apexchart height="350" type="radialBar" :options="chartOptions" :series="series"></apexchart>
+          </div>
+          <div class="keyword-tool-divider"></div>
+          <div class="top-chart-rect">
+            <div class="keyword-tool-label">
+              키워드 검색량
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on">help</v-icon>
+                </template>
+                <div style="padding: 10px;">
+                  키워드의 총 검색량을 보여드려, 해당 키워드를 사용자가 얼마나 검색을 하는지 확인할 수 있습니다.
+                  <br />*실시간데이터이기 때문에 점수가 변동될수있습니다.
+                </div>
+              </v-tooltip>
+            </div>
+            <apexchart height="350" type="radialBar" :options="chartOptions" :series="series"></apexchart>
+          </div>
+          <div class="keyword-tool-divider"></div>
+          <div class="top-chart-rect">
+            <div class="keyword-tool-label">
+              경쟁 컨텐츠량
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on">help</v-icon>
+                </template>
+                <div style="padding: 10px;">
+                  키워드를 검색했을때 온라인상에 노출되어지는 경쟁 컨텐츠량을 보여드려 얼마나 많은 경쟁 컨텐츠가 있는지 알려드립니다.
+                  <br />*실시간데이터이기 때문에 점수가 변동될수있습니다.
+                </div>
+              </v-tooltip>
+            </div>
+            <apexchart height="350" type="radialBar" :options="chartOptions" :series="series"></apexchart>
+          </div>
         </div>
         <div>
           <div id="half-donut"></div>
         </div>
-        <section>
+        <div class="mt50">
           <div class="keyword-tool-label">
-            키워드 Top3 노출 컨텐츠
-            <v-icon>help</v-icon>
+            키워드 Top3 노출 컨텐츠
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-icon v-on="on">help</v-icon>
+              </template>
+              <div style="padding: 10px;">
+                온라인상에 키워드 입력시 가장 먼저 화면상에 노출되어지는 컨텐츠 정보를 확인하여 경쟁자의 컨텐츠 퀄리티를 확인할 수 있습니다.
+                <br />* 실시간 데이터이기 때문에 순서가 변동될 수 있습니다.
+              </div>
+            </v-tooltip>
           </div>
-          <v-data-table
-            dark
-            :headers="headers"
-            :items="desserts"
-            class="elevation-1"
-          >
-            <template v-slot:items="props">
-              <td>{{ props.item.name }}</td>
-              <td class="text-xs-right">{{ props.item.calories }}</td>
-              <td class="text-xs-right">{{ props.item.fat }}</td>
-              <td class="text-xs-right">{{ props.item.carbs }}</td>
-              <td class="text-xs-right">{{ props.item.protein }}</td>
-              <td class="text-xs-right">{{ props.item.iron }}</td>
-            </template>
-          </v-data-table>
-        </section>
+        </div>
       </div>
     </section>
   </section>
@@ -76,159 +99,91 @@ export default {
     return {
       radios: "Youtube",
       keyword: "",
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "left",
-          sortable: false,
-          value: "name"
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
-      ],
-      series: [76],
+      colors: ["#1B5E20", "#7CB342", "#FDD835", "#F57C00", "#E64A19"],
+      series: [75],
       chartOptions: {
         chart: {
           type: "radialBar",
-          offsetY: -20,
-          sparkline: {
-            enabled: true
-          }
+          width: 200,
+          height: 200
         },
         plotOptions: {
           radialBar: {
             startAngle: -90,
             endAngle: 90,
             track: {
-              background: "#f0f0f0",
-              strokeWidth: "98%",
-              margin: 5 // margin is in pixels
+              background: "#f6f6f6"
+            },
+            hollow: {
+              size: "80%"
             },
             dataLabels: {
               name: {
-                show: true
+                color: "#888"
               },
               value: {
-                offsetY: -10,
-                fontSize: "30px"
+                offsetY: -40,
+                fontSize: "40px",
+                formatter: function(val) {
+                  return parseInt(val);
+                }
               }
             }
+          }
+        },
+        grid: {
+          padding: {
+            top: 20
           }
         },
         fill: {
           type: "gradient",
           gradient: {
-            shade: "light",
-            shadeIntensity: 0.4,
-            inverseColors: false,
-            opacityFrom: 1,
-            opacityTo: 1,
-            stops: [0, 25, 75, 100]
+            colorStops: [
+              {
+                offset: 0,
+                color: "#33691E"
+              },
+              {
+                offset: 20,
+                color: "#64DD17"
+              },
+              {
+                offset: 40,
+                color: "#FFD600"
+              },
+              {
+                offset: 60,
+                color: "#FFAB00"
+              },
+              {
+                offset: 80,
+                color: "#FF6D00"
+              },
+              {
+                offset: 100,
+                color: "#DD2C00"
+              }
+            ]
           }
+        },
+        stroke: {
+          lineCap: "round"
         },
         labels: ["Average Results"]
       }
     };
   },
   created() {
-    this.drawHalfDonot();
-
-    // https://v15.vuetifyjs.com/ko/components/data-tables
+    // https://apexcharts.com/vue-chart-demos/radialbar-charts/semi-circle-gauge/
+    // http://bl.ocks.org/headwinds/c8730e6df1ad408c3aa102d663a16cf5
+    // https://buefy.org/documentation/table/
     // https://vuetifyjs.com/ko/components/data-tables/
-    // https://zbciok.eu/java/index.php/2019/09/10/vue-js-v-data-tablevuetify-crud-example/
+    // https://www.materialui.co/colors
+    // https://gist.github.com/volodalexey/4e75aa8911c8bf764f1c169985c178b3
   },
   methods: {
-    search() {},
-    drawHalfDonot() {
-      // https://stackoverflow.com/questions/18198252/d3-arc-gradient
-      // http://bl.ocks.org/mbostock/5100636
-      // https://bl.ocks.org/adamjanes/53eedf0b915fd8b20f04fd08bc24ff00
-    }
+    search() {}
   }
 };
 </script>
@@ -240,10 +195,28 @@ export default {
 .keyword-tool-label {
   display: flex;
   color: $grey6;
+  align-self: start;
   font-size: 18px;
-
   & > .v-icon {
+    z-index: 10;
+    margin-left: 5px;
     cursor: pointer;
   }
+}
+
+.keyword-tool-divider {
+  width: 1px;
+  height: 150px;
+  background-color: $grey4;
+}
+
+.top-chart-rect {
+  width: calc(33.33% - 1px);
+  height: 220px;
+  display: flex;
+  padding: 0 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
